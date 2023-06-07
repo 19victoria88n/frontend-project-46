08cf07bde3e95ef0101a0f4bebfeba747dbc7139
Your Name <you@example.com>
2023-06-07 15:01:29 +0300

    correct

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -1,4 +1,3 @@
-/// outside imports
 import { test, expect } from '@jest/globals';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -12,11 +11,7 @@ const getFixturePath = (filename) => path.join(generatedName, '..', '__fixtures_
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test.each([
-  ['file1.yaml', 'file2.yaml', 'stylish', 'result-stylish.txt'],
   ['file1.json', 'file2.json', 'stylish', 'result-stylish.txt'],
-  ['file1.json', 'file2.json', 'json', 'result-json.json'],
-  ['file1.yaml', 'file2.yaml', 'json', 'result-json.json'],
-  ['file1.json', 'file2.json', 'plain', 'result-plain.txt'],
 ])('Comparing "%s" and "%s" with "%s" formatter equals to "%s"', (filename1, filename2, format, expected) => {
   const result = readFile(expected);
   const file1 = getFixturePath(filename1);
